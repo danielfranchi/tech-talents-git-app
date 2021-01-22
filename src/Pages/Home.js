8d12481016345caa1d988306916bc6ba2c3cd617
Daniel Franchi <daniel2mf@yahoo.com.br>
2021-01-22 01:51:38 -0300

    pasta pages do projeto

---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import MostrarGif from '../Components/MostrarGif'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Gif</h1>
+            <hr/>
+            
+            <MostrarGif /> <br/>
+        </div>
+    )
+}
+
+export default Home
